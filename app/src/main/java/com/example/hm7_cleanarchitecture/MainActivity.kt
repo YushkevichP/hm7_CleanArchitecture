2b74a4f1d84c4130ad8a777e6061b26730c2a47f
Pavel Yushkevich <7146514@gmail.com>
2022-06-21 16:47:53 +0300

    bottom nav menu done

--- app/src/main/java/com/example/hm7_cleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/example/hm7_cleanarchitecture/MainActivity.kt
@@ -2,23 +2,24 @@ package com.example.hm7_cleanarchitecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hm7_cleanarchitecture.fragments.BottomNavFragment
 import com.example.hm7_cleanarchitecture.googlemap.MapFragment
 import com.example.less21_androidcomponents.googlemap.CustomMapFragment
 
 
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+//class MainActivity : AppCompatActivity(R.layout.activity_main)
 
-//class MainActivity : AppCompatActivity(R.layout.activity_main) {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        supportFragmentManager
-//            .beginTransaction()
-//            .replace(R.id.container, MapFragment())
-//            .commit()
-//    }
-//}
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, BottomNavFragment())
+            .commit()
+    }
+}
 
 
 
