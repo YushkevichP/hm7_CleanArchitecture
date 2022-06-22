734f99ee0a60ddff98e3cc9cb72a5c3392745b37
Pavel Yushkevich <7146514@gmail.com>
2022-06-22 17:54:28 +0300

    marker on the map finally working

--- data/src/main/java/com/example/hm7_cleanarchitecture/data/model/ContryResponse.kt
+++ data/src/main/java/com/example/hm7_cleanarchitecture/data/model/ContryResponse.kt
@@ -6,32 +6,8 @@ import com.google.gson.annotations.SerializedName
 //todo можно заюзать флаг/столицу/ население(?)
 data class CountryResponse(
 
-    @SerializedName("capital")
-    val capital: List<String>,
-
-    @SerializedName("capitalInfo")
-    val capitalInfo: CapitalInfo,
-
-    @SerializedName("continents")
-    val continents: List<String>,
-
-    @SerializedName("currencies")
-    val currencies: Currencies,
-
-    @SerializedName("flag")
-    val flag: String,
-
-    @SerializedName("flags")
-    val flags: Flags,
-
-    @SerializedName("languages")
-    val languages: Languages,
-
     @SerializedName("latlng")
     val latlng: List<Double>,
-
-    @SerializedName("maps")
-    val maps: Maps,
 
     @SerializedName("name")
     val name: Name,
@@ -42,60 +18,40 @@ data class CountryResponse(
 
 data class CapitalInfo(
     @SerializedName("latlng")
-    val latlng: List<Double>
-)
-
-data class Currencies(
-    @SerializedName("USD")
-    val uSD: USD
+    val latlng: List<Double>,
 )
 
 data class Flags(
     @SerializedName("png")
     val png: String,
     @SerializedName("svg")
-    val svg: String
-)
-
-data class Languages(
-    @SerializedName("eng")
-    val eng: String
-)
-
-data class Maps(
-    @SerializedName("googleMaps")
-    val googleMaps: String,
-    @SerializedName("openStreetMaps")
-    val openStreetMaps: String
+    val svg: String,
 )
 
 data class Name(
     @SerializedName("common")
     val common: String,
-    @SerializedName("nativeName")
-    val nativeName: NativeName,
     @SerializedName("official")
-    val official: String
+    val official: String,
 )
 
-data class USD(
-    @SerializedName("name")
-    val name: String,
-    @SerializedName("symbol")
-    val symbol: String
-)
 
-data class NativeName(
-    @SerializedName("eng")
-    val eng: EngX
-)
+// добавить если поулчится в инфо по клику
+//    @SerializedName("capital")
+//    val capital: List<String>,
+//
+//    @SerializedName("capitalInfo")
+//    val capitalInfo: CapitalInfo,
 
-data class EngX(
-    @SerializedName("common")
-    val common: String,
-    @SerializedName("official")
-    val official: String
-)
+//    @SerializedName("flag")
+//    val flag: String,
+
+//    @SerializedName("flags")
+//    val flags: Flags,
+
+
+
+
 
 
 
